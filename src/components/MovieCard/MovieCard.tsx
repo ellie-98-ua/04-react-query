0f3765bb57012bd8b71380c560bf9dd594d96d21
Elvira Antoniuk <elviraantonuk5@gmail.com>
2025-09-11 20:43:08 +0100

    build

--- src/components/MovieCard/MovieCard.tsx
+++ src/components/MovieCard/MovieCard.tsx
@@ -1,4 +1,4 @@
-import React from "react";
+// import React from "react";
 import type { Movie } from "../../types/movie";
 import css from "./MovieCard.module.css";
 
@@ -7,18 +7,11 @@ interface MovieCardProps {
 }
 
 export default function MovieCard({ movie }: MovieCardProps) {
-  const posterUrl = movie.poster_path
-    ? `https://image.tmdb.org/t/p/w200${movie.poster_path}`
-    : "https://via.placeholder.com/200x300?text=No+Image";
-
   return (
-    <div className={css.card}>
-      <img className={css.poster} src={posterUrl} alt={movie.title} />
-      <div className={css.info}>
-        <h3 className={css.title}>{movie.title}</h3>
-        <p className={css.date}>{movie.release_date}</p>
-        <p className={css.overview}>{movie.overview}</p>
-      </div>
-    </div>
+    <li className={css.movieItem}>
+      <h3>{movie.title}</h3>
+      <p>{movie.release_date}</p>
+    </li>
   );
 }
+
